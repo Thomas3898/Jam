e6dd1fea72eb4c5f5bc068cf9dd273c4d73ab520
Thomas <grandin.thoma@gmail.com>
2022-05-20 18:48:25 +0200

    First Push

---
+++ New Unity Project/Assets/Scripts/Symbole.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Symbole : MonoBehaviour
+{
+    public Sprite[] symbole;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        for (int i = 0; i < symbole.Length; i++)
+        {
+
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
